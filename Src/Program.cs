272911bc3e87588848c59639794048106ee91d55
don logan <sleepyparadox@gmail.com>
2016-07-23 14:39:48 +1000

    initial check in

---
+++ Src/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncFromUrl
+{
+    class Program
+    {
+        static void Main(string[] argsRaw)
+        {
+            var configPath = "config.txt";
+            Config config;
+            
+            if(File.Exists(configPath))
+                config = Config.FromFile(configPath);
+            else
+            {
+                config = Config.Default;
+                config.Save(configPath);
+            }
+
+            if (string.IsNullOrWhiteSpace(config.LocalDirectory))
+                config.LocalDirectory = Directory.GetCurrentDirectory();
+
+            var interval = 1000;
+            if (!int.TryParse(config.SyncInterval, out interval))
+                Console.WriteLine(string.Format("failed to parse SyncInterval {0} as int", config.SyncInterval));
+
+            SyncFromUrl.ContinouslySync(config.QueryUrl, config.FileHostUrl, config.LocalDirectory, interval);
+        }
+    }
+}
---
+++ Src/SyncFromUrl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncFromUrl
+{
+    public static class SyncFromUrl
+    {
+        public static void ContinouslySync(string queryUrl, string fileHost, string localDirectory, int interval)
+        {
+            Console.WriteLine("Begin ContinousSync");
+            while (true)
+            {
+                try
+                {
+                    Sync(queryUrl, fileHost, localDirectory);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                Thread.Sleep(interval);
+            }
+        }
+
+        public static void Sync(string queryUrl, string fileHost, string localDirectory)
+        {
+            using (var client = new WebClient())
+            {
+                var syncInfo = client.DownloadString(queryUrl);
+                var syncFiles = syncInfo.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach(var syncPath in syncFiles)
+                {
+                    var localPath = Path.Combine(localDirectory, syncPath.Replace("/", "\\"));
+                    if(File.Exists(localPath))
+                        continue;
+
+                    Console.WriteLine("found " + syncPath);
+
+                    var localPathsDirectory = localPath.Substring(0, localPath.LastIndexOf('\\'));
+                    if (!Directory.Exists(localPathsDirectory))
+                        Directory.CreateDirectory(localPathsDirectory);
+                    
+                    var serverPath = Path.Combine(fileHost, syncPath);
+                    var localTempPath = localPath + ".SyncFromUrl.TEMP";
+                    client.DownloadFile(serverPath, localTempPath);
+                    File.Move(localTempPath, localPath);
+
+                    Console.WriteLine("downloaded to " + localPath);
+                }
+            }
+        }
+    }
+}
